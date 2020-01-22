2aad4dbe9847f87009030271dd551632bcf9d3dd
EC2 Default User <ec2-user@ip-172-31-42-116.us-east-2.compute.internal>
2020-01-22 06:51:31 +0000

    18の課題

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\HTML;
 
-use App\News;
+use App\Profile;
 
 class NewsController extends Controller
 {
     //
     public function index(Request $request)
     {
-        $posts = News::all()->sortByDesc('updated_at');
+        $posts = Profile::all()->sortByDesc('updated_at');
         
         if (count($posts) > 0) {
             $headline = $posts->shift();
@@ -20,6 +20,6 @@ class NewsController extends Controller
             $headline = null;
         }
         
-        return view('news.index', ['headline' => $headline, 'posts' => $posts]);
+        return view('profile.index', ['headline' => $headline, 'posts' => $posts]);
     }
 }
